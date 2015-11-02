878fff64f4f7219a877137b0df99bf715150f675
Erik Beeson <erik.beeson@gmail.com>
2015-11-01 20:47:00 -0800

    Add javadocs to data objects
    Comments taken from [zxcvbn README.md](https://github.com/dropbox/zxcvbn/blob/ab6da8a50a7ff313f99ea09a6a82fd488f5a1d62/README.md)

--- src/main/java/wtf/password/PasswordStrength.java
+++ src/main/java/wtf/password/PasswordStrength.java
@@ -18,17 +18,27 @@ public class PasswordStrength {
 	public PasswordStrength() {
 	}
 
+	/**
+	 * @return Estimated guesses needed to crack password.
+	 */
 	public double getGuesses() {
 		return guesses;
 	}
 
+	/**
+	 * @return Order of magnitude of {@link #getGuesses}.
+	 */
 	public float getGuessesLog10() {
 		return guessesLog10;
 	}
 
+	/**
+	 * @return Dictionary of back-of-the-envelope crack time estimations, in seconds, based on a few scenarios...
+	 */
 	public CrackTimeSeconds getCrackTimeSeconds() {
 		return crackTimeSeconds;
 	}
+
 
 	public CrackTimeDisplay getCrackTimeDisplay() {
 		return crackTimeDisplay;
@@ -42,6 +52,9 @@ public class PasswordStrength {
 		return "PasswordStrength{" + "guesses=" + guesses + ", guessesLog10=" + guessesLog10 + ", crackTimeSeconds=" + crackTimeSeconds + ", crackTimeDisplay=" + crackTimeDisplay + ", score=" + score + '}';
 	}
 
+	/**
+	 * Dictionary of back-of-the-envelope crack time estimations, in seconds, based on a few scenarios...
+	 */
 	public static class CrackTimeSeconds {
 		@SerializedName("online_throttling_100_per_hour")
 		private double onlineThrottling100PerHour;
@@ -55,27 +68,48 @@ public class PasswordStrength {
 		public CrackTimeSeconds() {
 		}
 
+		/**
+		 * @return Online attack on a service that ratelimits password auth attempts.
+		 */
 		public double getOnlineThrottling100PerHour() {
 			return onlineThrottling100PerHour;
 		}
 
+		/**
+		 * @return Online attack on a service that doesn't ratelimit, or where an attacker has outsmarted ratelimiting.
+		 */
 		public double getOnlineNoThrottling10PerSecond() {
 			return onlineNoThrottling10PerSecond;
 		}
 
+		/**
+		 * @return Offline attack. Assumes multiple attackers, proper user-unique salting, and a slow hash function w/ moderate work factor, such as bcrypt, scrypt, PBKDF2.
+		 */
 		public double getOfflineSlowHashing1e4PerSecond() {
 			return offlineSlowHashing1e4PerSecond;
 		}
 
+		/**
+		 * @return Offline attack with user-unique salting but a fast hash function like SHA-1, SHA-256 or MD5. A wide range of reasonable numbers anywhere from one billion - one trillion guesses per second, depending on number of cores and machines. Ballparking at 10B/sec.
+		 */
 		public double getOfflineFastHashing1e10PerSecond() {
 			return offlineFastHashing1e10PerSecond;
 		}
 
 		public String toString() {
-			return "CrackTimeSeconds{" + "onlineThrottling100PerHour=" + onlineThrottling100PerHour + ", onlineNoThrottling10PerSecond=" + onlineNoThrottling10PerSecond + ", offlineSlowHashing1e4PerSecond=" + offlineSlowHashing1e4PerSecond + ", offlineFastHashing1e10PerSecond=" + offlineFastHashing1e10PerSecond + '}';
+			return "CrackTimeSeconds{" +
+						   "onlineThrottling100PerHour=" + onlineThrottling100PerHour + ", " +
+						   "onlineNoThrottling10PerSecond=" + onlineNoThrottling10PerSecond + ", " +
+						   "offlineSlowHashing1e4PerSecond=" + offlineSlowHashing1e4PerSecond + ", " +
+						   "offlineFastHashing1e10PerSecond=" + offlineFastHashing1e10PerSecond + "}";
 		}
 	}
 
+	/**
+	 * Same keys as {@link CrackTimeSeconds}, with friendlier display string values: "less than a second", "3 hours", "centuries", etc.
+	 *
+	 * @see wtf.password.PasswordStrength.CrackTimeSeconds
+	 */
 	public static class CrackTimeDisplay {
 		@SerializedName("online_throttling_100_per_hour")
 		private String onlineThrottling100PerHour;
@@ -89,24 +123,40 @@ public class PasswordStrength {
 		public CrackTimeDisplay() {
 		}
 
+		/**
+		 * @return Online attack on a service that ratelimits password auth attempts.
+		 */
 		public String getOnlineThrottling100PerHour() {
 			return onlineThrottling100PerHour;
 		}
 
+		/**
+		 * @return Online attack on a service that doesn't ratelimit, or where an attacker has outsmarted ratelimiting.
+		 */
 		public String getOnlineNoThrottling10PerSecond() {
 			return onlineNoThrottling10PerSecond;
 		}
 
+		/**
+		 * @return Offline attack. Assumes multiple attackers, proper user-unique salting, and a slow hash function w/ moderate work factor, such as bcrypt, scrypt, PBKDF2.
+		 */
 		public String getOfflineSlowHashing1e4PerSecond() {
 			return offlineSlowHashing1e4PerSecond;
 		}
 
+		/**
+		 * @return Offline attack with user-unique salting but a fast hash function like SHA-1, SHA-256 or MD5. A wide range of reasonable numbers anywhere from one billion - one trillion guesses per second, depending on number of cores and machines. Ballparking at 10B/sec.
+		 */
 		public String getOfflineFastHashing1e10PerSecond() {
 			return offlineFastHashing1e10PerSecond;
 		}
 
 		public String toString() {
-			return "CrackTimeDisplay{" + "onlineThrottling100PerHour='" + onlineThrottling100PerHour + '\'' + ", onlineNoThrottling10PerSecond='" + onlineNoThrottling10PerSecond + '\'' + ", offlineSlowHashing1e4PerSecond='" + offlineSlowHashing1e4PerSecond + '\'' + ", offlineFastHashing1e10PerSecond='" + offlineFastHashing1e10PerSecond + '\'' + '}';
+			return "CrackTimeDisplay{" +
+						   "onlineThrottling100PerHour='" + onlineThrottling100PerHour + "', " +
+						   "onlineNoThrottling10PerSecond='" + onlineNoThrottling10PerSecond + "', " +
+						   "offlineSlowHashing1e4PerSecond='" + offlineSlowHashing1e4PerSecond + "', " +
+						   "offlineFastHashing1e10PerSecond='" + offlineFastHashing1e10PerSecond + '}';
 		}
 	}
 }
